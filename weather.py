bdfbfe3edb0346f72579719f45799221dc5fb432
Alberto Alva <albertoaaa99@gmail.com>
2024-04-10 20:43:24 -0600

    Update App

--- weather.py
+++ weather.py
@@ -1,6 +1,6 @@
 import requests
 import math
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -23,25 +23,5 @@ def index():
     main = data["weather"][0]["main"]
 
     return render_template('clima.html', ciudad=ciudad, temperatura=temperatura, velocidad_viento=velocidad_viento, latitud=latitud, longitud=longitud, descripcion=descripcion, main=main)
-
-@app.route('/clima', methods=['POST'])
-def clima():
-    ciudad = request.form['ciudad']
-    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=e97d4e183422cfadb9e54db519a092f3&units=metric".format(ciudad)
-    res = requests.get(url)
-    data = res.json()
-
-    temperatura = data["main"]["temp"]
-    temperatura = math.trunc(temperatura)
-    velocidad_viento = data["wind"]["speed"]
-
-    latitud = data["coord"]["lat"]
-    longitud = data["coord"]["lon"]
-
-    descripcion = data["weather"][0]["description"]
-    main = data["weather"][0]["main"]
-
-    return render_template('clima.html', ciudad=ciudad, temperatura=temperatura, velocidad_viento=velocidad_viento, latitud=latitud, longitud=longitud, descripcion=descripcion, main=main)
-
 if __name__ == '__main__':
     app.run(port = 8000)
